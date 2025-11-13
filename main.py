50596dbe84d397ddb9292e3b58c58de1e2874444
crassicus <carlostobon@tutanota.de>
2025-11-13 09:28:27 -0500

    third commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import math
+
+
 def main():
     print("Hello from example!")
 
 
 def add(x: int, y: int) -> int:
     return x + y
+
+
+def logarithm(x: float) -> int:
+    return math.log(x)
 
 
 if __name__ == "__main__":
